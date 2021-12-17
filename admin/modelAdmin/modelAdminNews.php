5a07e40d4176476e5049da6da655be2d394bd0f2
GennadiPyton <zikito777@mail.ru>
2021-12-17 17:55:51 +0200

    vesrion 8 admin delete

--- admin/modelAdmin/modelAdminNews.php
+++ admin/modelAdmin/modelAdminNews.php
@@ -77,7 +77,21 @@ public static function getNewsEdit($id){
                 $test=true;
             }
         }
+    } 
+}
+//-------------------------------------------news delete
+
+public static function getNewsDelete($id) {
+    $test=false;
+    if(isset($_POST['save'])) {
+        $sql="DELETE FROM `news` WHERE `news`.`id` = ".$id;
+        $db = new Database();
+                $item = $db->executeRun ($sql);
+            if($item==true) {
+            $test=true;
+        }
     }
     return $test;
-}
+    }
+
 }//class
